e23d9beea619b491dc115c58a7a42cb7778a3371
Alexey Golubev <Alexey.Golubev@onlyoffice.com>
2016-12-28 18:07:36 +0300

    Reworked setup params

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 INVENTORY=staging
-#PLAYBOOKS+=(filestorage.yaml)
-#PLAYBOOKS+=(loadbalancer.yaml)
-#PLAYBOOKS+=(database.yaml)
-#PLAYBOOKS+=(rabbitmq.yaml)
-#PLAYBOOKS+=(redis.yaml)
-PLAYBOOKS+=(documentservers.yaml)
-PLAYBOOKS+=(documentserver-example.yaml)
 
 sudo ansible-galaxy install -r requirements.yaml
 
-for i in ${PLAYBOOKS[@]};
-do
-  ansible-playbook ${i} -i ${INVENTORY}
-done
+ansible-playbook playbook.yaml -i ${INVENTORY}
